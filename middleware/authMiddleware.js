a9f3dba8a5f024e79aeb88e82a76e48b0765b823
JoshuaSowerby <josh.d.sowerby@gmail.com>
2025-08-25 16:23:14 +1000

    generic jwt code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+
 exports.verifyToken = async (req, res, next) =>{
     console.log('auth not implemented');
     next();
+    //////////////////
+    if (false){
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")){
+            return res.status(401).json({message: 'Access denied, no token provided'});
+        }
+        const token = authHeader.split(' ')[1];
+
+        try {
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            req.user_id = decoded;
+            next();
+        }catch (err){
+            return res.status(401).json({ message: 'Invalid or expired token' });
+        }
+    } 
+    
 };
